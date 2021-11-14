86979867e683753e0579be421be02ec031459cb7
davidelucchin <davide.lucchin@berenice.it>
2021-11-14 17:29:41 +0100

    modal layout and validation working

--- src/app/features/card-list/card-form-modal/card-form-modal.component.ts
+++ src/app/features/card-list/card-form-modal/card-form-modal.component.ts
@@ -10,6 +10,7 @@ import { Card } from '../../../shared/models/card.model';
   styleUrls: ['./card-form-modal.component.scss']
 })
 export class CardFormModalComponent implements OnInit{
+  console = console
   form = this.fb.group({
     //hiddent properties
     _id:[''],
@@ -21,8 +22,8 @@ export class CardFormModalComponent implements OnInit{
     surname: ['', [Validators.required]],
     number: ['', [
       Validators.required,
-      Validators.minLength(16),
-      Validators.maxLength(16),
+      Validators.minLength(19),
+      Validators.maxLength(19),
     ]],
     code: ['', [
       Validators.required,
@@ -31,6 +32,8 @@ export class CardFormModalComponent implements OnInit{
     ]],
     type: ['', [Validators.required]],
   });
+  pattern = /[^0-9]/g;
+
 
   constructor(
     private fb: FormBuilder,
@@ -44,7 +47,7 @@ export class CardFormModalComponent implements OnInit{
         name: card.owner.split(' ')[0],
         surname: card.owner.split(' ')[1],
         amount: card.amount,
-        number: creditCardNormalizer(card.number),
+        number: creditCardVisualizer(card.number),
         type: card.type,
         code: ''
       })
@@ -62,10 +65,18 @@ export class CardFormModalComponent implements OnInit{
     this.form.value.amount = this.form.value.amount ?? 0;
   }
 
-  transform() {
+  transformNumber(value: string) {
+    value = value.replace(this.pattern, "");
     if (this.form && this.form.get('number')) {
-      let transformValue = creditCardVisualizer(this.form.get('number')!.value);
+      let transformValue = creditCardVisualizer(value);
       this.form.get('number')!.setValue(transformValue);
+    }
+  }
+
+  transformCode(value: string) {
+    value = value.replace(this.pattern, "");
+    if (this.form && this.form.get('code')) {
+      this.form.get('code')!.setValue(value);
     }
   }
 }
@@ -73,6 +84,7 @@ export class CardFormModalComponent implements OnInit{
 function creditCardNormalizer(number: string): string {
   return number.split(' ').join('');
 }
+
 function creditCardVisualizer(number: string): string {
   number = creditCardNormalizer(number);
   if (number.length < 5)
@@ -84,8 +96,14 @@ function creditCardVisualizer(number: string): string {
 
   if (number.length < 13)
     return number.substring(0, 4) + ' ' +
-      number.substring(4, 9) + ' ' +
-      number.substring(9, number.length);
+      number.substring(4, 8) + ' ' +
+      number.substring(8, number.length);
+
+  if (number.length < 17)
+    return number.substring(0, 4) + ' ' +
+      number.substring(4, 8) + ' ' +
+      number.substring(8, 12) + ' ' +
+      number.substring(12, number.length);
 
   // nubmer.length > 16
   return number.substring(0, 4) + ' ' +
