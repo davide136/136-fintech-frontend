c48d4f0d92f666a9427740ab1213c29c4852d6ca
davidelucchin <davide.lucchin@berenice.it>
2021-12-12 19:14:52 +0100

    activelink as behaviour subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -89,7 +89,7 @@ var appMenu: Link[] = [
 })
 export class AppComponent {
   @ViewChild('drawer', { static: true }) drawer!: MatDrawer;
-  activeLinkIndex = -1;
+  activeLinkIndex$ = new BehaviorSubject<number>(-1);
   getScreenWidth: number = 0;
   MIN_WIDTH_FOR_SHOWING_MENU = MIN_WIDTH_FOR_SHOWING_MENU;
   loggedUser$ = new BehaviorSubject<User | null>(null);
@@ -111,11 +111,12 @@ export class AppComponent {
       this.drawer.toggle(true);
 
     this.router.events.subscribe(() => {
-      this.activeLinkIndex = this.navLinks$.value.indexOf(
-        this.navLinks$.value.find(tab => tab.link === '.' + this.router.url)!);
+      this.activeLinkIndex$.next(this.navLinks$.value.indexOf(
+        this.navLinks$.value.find(tab => tab.link === '.' + this.router.url)!));
     });
     this.userStore.user$.pipe(
       map(user => {
+        this.activeLinkIndex$.next(-1);
         this.loggedUser$.next(user);
         if (user) {
           appMenu[6].user = user.displayName;
